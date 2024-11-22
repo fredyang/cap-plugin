9d958fd4538731ac27dd05194ec845326aeec1c5
Fred Yang <fredyang@semanticsworks.com>
2024-11-22 00:52:30 -0500

    save

--- Package.swift
+++ Package.swift
@@ -2,27 +2,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "CapPlugin",
-    platforms: [.iOS(.v13)],
-    products: [
-        .library(
-            name: "CapPlugin",
-            targets: ["ExamplePlugin"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
-    ],
-    targets: [
-        .target(
-            name: "ExamplePlugin",
-            dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
-            ],
-            path: "ios/Sources/ExamplePlugin"),
-        .testTarget(
-            name: "ExamplePluginTests",
-            dependencies: ["ExamplePlugin"],
-            path: "ios/Tests/ExamplePluginTests")
-    ]
+  name: "CapPlugin",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      // the library CapPlugin use target ExamplePlugin
+      name: "CapPlugin",
+      targets: ["ExamplePlugin"])
+  ],
+  dependencies: [
+    // the package name is capacitor-swift-pm
+    .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
+    // the package name is verify-sdk-ios
+    .package(url: "https://github.com/ibm-security-verify/verify-sdk-ios", branch: "main"),
+    .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.0")),
+  ],
+  targets: [
+    // target is configuration
+    .target(
+      name: "ExamplePlugin",
+      // the
+      dependencies: [
+        .product(name: "Capacitor", package: "capacitor-swift-pm"),
+        .product(name: "Cordova", package: "capacitor-swift-pm"),
+        .product(name: "MFA", package: "verify-sdk-ios"),
+        .product(name: "Alamofire", package: "Alamofire"),
+      ],
+      // the source code is in this folder
+      path: "ios/Sources/ExamplePlugin"
+    ),
+
+    .testTarget(
+      name: "ExamplePluginTests",
+      dependencies: ["ExamplePlugin"],
+      path: "ios/Tests/ExamplePluginTests"),
+  ]
 )
--- ios/Sources/ExamplePlugin/Example.swift
+++ ios/Sources/ExamplePlugin/Example.swift
@@ -1,8 +1,22 @@
+import Alamofire
 import Foundation
+import MFA
 
 @objc public class Example: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+  @objc public func echo(_ value: String) -> String {
+    print(value)
+    return value
+  }
+
+  @objc func fetchData(_ call: CAPPluginCall) {
+    AF.request("https://example.com").response { response in
+      if let data = response.data {
+        call.resolve([
+          "data": String(data: data, encoding: .utf8) ?? ""
+        ])
+      } else {
+        call.reject("Failed to fetch data")
+      }
     }
+  }
 }
